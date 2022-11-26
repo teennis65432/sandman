579b8e78cdb64289b7fc7430f53bdd4ed7b12bd7
Ricky Herget <hergetr19@students.ecu.edu>
2022-11-25 22:10:03 -0500

    add manager tools

--- src/tables.py
+++ src/tables.py
@@ -1,50 +1,58 @@
 from webapp import app, db
 from src.models import User
 
+
 def createTables():
     with app.app_context():
         db.create_all()
+
 
 def getTables():
     with app.app_context():
         return db.engine.table_names()
 
+
 def getColumns(table):
     with app.app_context():
         if not db.engine.has_table(table):
-                return 'table ' + table + ' does not exist'
+            return "table " + table + " does not exist"
         conn = db.engine.connect()
-        return conn.execute('SELECT * FROM ' + table).keys()
+        return conn.execute("SELECT * FROM " + table).keys()
+
 
 def addUser(name, password, manager):
-        newUser = User(name=name, password=password, manager=manager)
+    newUser = User(name=name, password=password, manager=manager)
+    with app.app_context():
+        db.session.add(newUser)
+        db.session.commit()
+
+
+def removeUser(id):
+        user = getUser(id)
         with app.app_context():
-                db.session.add(newUser)
+                db.session.delete(user)
                 db.session.commit()
 
-def getAllUsers():
-        with app.app_context():
-                users = User.query.all()
 
-                for user in users:
-                        print(user)
-                        print(user.name)
+def getAllUsers():
+    with app.app_context():
+        users = User.query.all()
+        return users
+
 
 def getUser(id):
-        with app.app_context():
-                employee = User.query.filter_by(id=id).first()
-                return employee
-                
-        return None
+    with app.app_context():
+        employee = User.query.filter_by(id=id).first()
+        return employee
+
+    return None
+
 
 def checkLogin(user, password):
-        with app.app_context():
-                employee = User.query.filter_by(name=user).first()
-                if (employee is not None):
-                        print(employee)
-                        print(employee.password)
-                        if(employee.password == password):
-                                return True
-                
-        return False
-        
+    with app.app_context():
+        employee = User.query.filter_by(name=user).first()
+        if employee is not None:
+            if employee.password == password:
+                return employee
+
+    return None
--- webapp.py
+++ webapp.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_login import LoginManager, login_required, logout_user
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 import src.calendarHelper as calendarHelper
 import src.tables as tables
-
 
 LM = LoginManager()
 db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.database'
+app.secret_key = 'a secret shhhh'
 
 LM.init_app(app)
 db.init_app(app)
@@ -18,25 +18,72 @@ db.init_app(app)
 def load_user(user_id):
     return tables.getUser(user_id)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def start():
+    if current_user == None:
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     error = None
     if request.method == 'POST':
-        if not tables.checkLogin(request.form['username'], request.form['password']):
+        user = tables.checkLogin(request.form['username'], request.form['password'])
+        if user == None:
             error = 'Invalid Credentials. Please try again.'
         else:
+            login_user(user)
             return redirect(url_for('home'))
-    return render_template("index.html", error=error)
+    return render_template("login.html", error=error)
 
 @app.route('/home')
+@login_required
 def home():
-    return render_template("Home.html", month=calendarHelper.getCurMonth())
+    return render_template("Home.html", month=calendarHelper.getCurMonth(), user=current_user)
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect('')
+    return redirect(url_for('login'))
+
+@app.route('/add-employee', methods=['GET', 'POST'])
+@login_required
+def addEmployee():
+    if not current_user.manager:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        manager = False
+        if request.form['manager'] == 'True':
+            manager = True
+        tables.addUser(request.form['username'], request.form['password'], manager)
+        return redirect(url_for('allEmployees'))
+    return render_template('add-employee.html')
+    
+@app.route('/all-employees')
+@login_required
+def allEmployees():
+    if not current_user.manager:
+        return redirect(url_for('home'))
+    return render_template('all-employees.html', users=tables.getAllUsers())
+
+
+@app.route('/remove-employee')
+@login_required
+def removeEmployee():
+    if not current_user.manager:
+        return redirect(url_for('home'))
+    return render_template('remove-employee.html', users=tables.getAllUsers())
+    
+@app.route('/remove/<id>')
+@login_required
+def remove(id):
+    if not current_user.manager:
+        return redirect(url_for('home'))
+
+    tables.removeUser(id)
+    return redirect(url_for('removeEmployee'))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
